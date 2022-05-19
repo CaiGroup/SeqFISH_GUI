3a394cf153f03d3c5b1cf75d99f2bb7a78d88364
sskhon-2014 <sskhon2014@berkeley.edu>
2022-05-18 19:48:15 -0700

    another one

--- seqFISH_DASH/post_processing/decoding_files/run.py
+++
@@ -1,5 +0,0 @@
-import time
-import os
-
-time.sleep(60*60*2)
-os.system('sbatch dash_radial_decoding_BEFORE_OP.batch')
--- seqFISH_DASH/pre_processing/preprocessing_files/run_command.py
+++
@@ -1,5 +0,0 @@
-import time
-import os
-
-time.sleep(60*60*0.5)
-os.system('sbatch correct_many.batch')
